4fa03d82e7f68d80e3ac306d1e08dd76e2fd27ce
Ddg80 <dguillin80@gmail.com>
2024-02-09 18:42:29 +0100

    path button to paymens

--- lib/utils/widgets/buttons/button_large.dart
+++ lib/utils/widgets/buttons/button_large.dart
@@ -40,6 +40,12 @@ class _ButtonLargeState extends State<ButtonLarge> {
               builder: (_) => widget.page,
             ),
           );
+        } else {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => widget.page,
+            ),
+          );
         }
       },
       child: Row(
